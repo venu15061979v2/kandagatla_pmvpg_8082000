a2e861cbf7b7c9681f5339ebbc4a54d2b62ff375
venu15061979v2 <31860232+venu15061979v2@users.noreply.github.com>
2017-09-11 22:35:17 +0530

    no errors now

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -32,7 +32,7 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void setStudents(Student[] students) {
 		// Add your implementation here
-		this.students = students
+		this.students = students;
 	}
 
 	@Override
